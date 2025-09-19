57cb6577ba74c42bf00d02fea5a08b9e9e6a9c97
professorAndre <prof.andre.simao@hotmail.com>
2025-09-19 13:13:53 -0300

    Primeiro commit do projeto suporte

---
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
@@ -0,0 +1,39 @@
+package com.eventoapp.eventoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventoapp.eventoapp.model.Evento;
+import com.eventoapp.eventoapp.repository.EventoRepository;
+
+@Controller
+public class EventoController {
+	
+	@Autowired
+	private EventoRepository er;
+	
+	@GetMapping(value ="/cadastrarEvento")
+	public String form() {
+		return "evento/formEvento";
+	}
+	@PostMapping(value ="/cadastrarEvento")
+	public String form(Evento evento) {
+		
+		er.save(evento);
+		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("/index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
+
+}
